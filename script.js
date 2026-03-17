096327168dbf6bab60c1625f624f4d36c0186f8a
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-18 00:08:51 +0530

    fundamental of javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+// alert("Helllo");
+
+var arr=[1,2,3,4];
+
+//? ForEach Loop
+// arr.forEach(function(val){
+//     console.log(val+" Hello world");
+// })
+
+
+//? Map Meathod
+// var ans=arr.map(function(val){
+//     return val*3;
+// })
+// console.log(ans);
+
+//? Filter Meathod
+// var ans=arr.filter(function(val){
+//     if(val>3){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// })
+// console.log(ans);
+
+//? Find meathod
+// var ans=arr.find(function(val){
+//     if(val==2){
+//         return val;
+//     }
+// })
+// console.log(ans);
+
+
+//? index of
+// console.log(arr.indexOf(32));
